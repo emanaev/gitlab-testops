fe5f962ccfdc626d83ac6ad8101d613e92d0215e
Eugene Manaev <emanaev@gmail.com>
2024-03-10 11:57:09 +0300

    First commit

---
+++ ops/app/main.py
@@ -0,0 +1,32 @@
+from typing import Union
+from fastapi import FastAPI
+import gitlab
+import time
+import logging
+logger = logging.getLogger(__name__)
+
+
+GITLAB_URL='http://gitlab'
+
+
+gl = gitlab.Gitlab(GITLAB_URL, private_token='token-string-here123')
+logger.info(f"Waiting for GITLAB [{GITLAB_URL}] ...")
+while True:
+    try:
+        gl.auth()
+        break
+    except:
+        time.sleep(3)  
+logger.info("GITLAB is up!")
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
